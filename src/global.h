ade27934ae3e350de5ce50aa837310fbb8af253b
Skkeye <skkeye@gmail.com>
2023-03-16 15:03:39 -0400

    ajout de global.h

---
+++ src/global.h
@@ -0,0 +1,9 @@
+/// @file headers.h
+/// @brief Les definitions globales incluant les entetes de fonction
+///
+/// Ce fichier contient les entetes de fonction et d'autre definitions globales
+/// necessare au project
+
+/// Olivier
+
+/// Yanick
