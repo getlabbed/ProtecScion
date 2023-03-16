bee87b4401fe8862b36cf400e0e2131aad4b2a7a
Skkeye <skkeye@gmail.com>
2023-03-16 15:16:13 -0400

    section de global.h

--- src/global.h
+++ src/global.h
@@ -6,4 +6,25 @@
 
 /// Olivier
 
+/// Affichage de la temperature du bois
+float readTemperatureWood();
+
+/// Programmation des indicateurs lumineux
+enum lightColors { Red = 1, Green = 2, Blue = 4 };
+void setLightIndicator(lightColors);
+
+/// Programmation du mode operation (asservissement, anti-recul)
+
+/// Programmation du mode manuel
+
+/// Journalisation
+
 /// Yanick
+
+/// Programmation du mode modification
+
+/// Programmation du mode apprentissage
+
+/// Afficher le niveau sonore en dB
+
+/// Afficher la temperature et l'humidite ambiante
