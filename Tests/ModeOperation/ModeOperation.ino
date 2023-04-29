7dfe74e8d29b50f56feaed289fd820a39d858fe0
getlabbed@proton.me <1927941@cegepoutaouais.qc.ca>
2023-04-29 13:34:31 -0400

    feat: Test Sound sensor

--- Tests/ModeOperation/ModeOperation.ino
+++ Tests/ModeOperation/ModeOperation.ino
@@ -1,13 +1,13 @@
 
 #include <PID_v2.h>
 
-#define PIN_MOTOR 25
-#define PIN_SENSE 4
+#define PIN_MOTOR 26
+#define PIN_SENSE 25
 
 double Kp = 2, Ki = 5, Kd = 1;
 PID_v2 myPID(Kp, Ki, Kd, PID::Direct);
 
-bool isFastChange(double input, int threshold = 100);
+bool isFastChange(double input, int threshold);
 
 static double lastInput = 0;
 
@@ -26,7 +26,7 @@ void loop()
 
   //Serial.println(input);
 
-  if (isFastChange(input))
+  if (isFastChange(input, 100))
   {
     Serial.println("Anti-recul triggered");
   }
