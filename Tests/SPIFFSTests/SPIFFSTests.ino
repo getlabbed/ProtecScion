63b5c59408fc3ef4d5eb54416e81a28c8774647c
Skkeye <skkeye@gmail.com>
2023-04-29 16:18:20 -0400

    Co-authored-by: Yanick Labelle <getlabbed@users.noreply.github.com>

--- Tests/SPIFFSTests/SPIFFSTests.ino
+++ Tests/SPIFFSTests/SPIFFSTests.ino
@@ -1,139 +1,247 @@
-// write me an arduino program that will have a simple file browser on a web page
-#include <Arduino.h>
-#include <SPIFFS.h>
-#include <WiFi.h>
-#include <WiFiServer.h>
-#include <FS.h>
+// Mode manuel: ajouter un bois si un bois n
+//
+//
 
 
-// Replace with your network credentials
-const char* ssid = "ESP32-Access-Point";
-const char* password = "123456789";
+#include "FS.h"
+#include "SPIFFS.h"
+#include <ArduinoJson.h>
 
-WiFiServer server(80);
+typedef struct
+{
+	char name[16]; // nom du bois
+	int code;			 // code du bois
+	int sawSpeed;	 // vitesse de la lame
+	int feedRate;	 // vitesse d'avancement
+} Wood_t;
 
-// Variable to store the HTTP request
-String header;
+// Types de bois disponibles
+const String woodType[] = {"Sapin", "Pin", "Noyer", "Bouleau", "Chêne", "Érable", "Frêne", "Merisier", "Mélèze", "Épinette"};
 
-// Auxiliar variables to store the current output state
-String outputState = "off";
+void writeEmptyFile();
+void readWood(Wood_t wood, int id);
+void writeWood(Wood_t wood, int id, int sawSpeed, int feedRate);
+void updateWood(Wood_t wood, int id, int sawSpeed, int feedRate);
 
-// Assign output variables to GPIO pins
-const int output = 2;
+void setup()
+{
+	Serial.begin(115200);
+	if (!SPIFFS.begin(true))
+	{
+		Serial.println("An error has occurred while mounting SPIFFS");
+	}
+	else
+	{
+		writeEmptyFile();
+		Wood_t wood;
+		writeWood(wood, 1, 100, 100);
+		readWood(wood, 1);
+		Serial.println(wood.name);
+		Serial.println(wood.code);
+		Serial.println(wood.sawSpeed);
+		Serial.println(wood.feedRate);
 
-void setup() {
-  Serial.begin(115200);
-  // Initialize the output variables as outputs
-  pinMode(output, OUTPUT);
-  // Set outputs to LOW
-  digitalWrite(output, LOW);
+		updateWood(wood, 1, 200, 200);
 
-  // Connect to Wi-Fi network with SSID and password
-  Serial.print("Setting AP (Access Point)…");
-  // Remove the password parameter, if you want the AP (Access Point) to be open
-  WiFi.softAP(ssid, password);
-
-  IPAddress IP = WiFi.softAPIP();
-  Serial.print("AP IP address: ");
-  Serial.println(IP);
-
-  server.begin();
+		wood = readWood(wood, 1);
+		// print the return of the function (struct)
+		Serial.println(wood.name);
+		Serial.println(wood.code);
+		Serial.println(wood.sawSpeed);
+		Serial.println(wood.feedRate);
+		}
 }
 
-void loop(){
-  WiFiClient client = server.available();   // Listen for incoming clients
+void loop()
+{
+	// Ne rien faire
+}
 
-  if (client) {                             // If a new client connects,
-    Serial.println("New Client.");          // print a message out in the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected()) {            // loop while the client's connected
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out the serial monitor
-        header += c;
-        if (c == '\n') {                    // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println();
+void writeEmptyFile()
+{
+	File file = SPIFFS.open("/wood.json", "w");
+	if (!file)
+	{
+		Serial.println("Failed to create file");
+		return;
+	}
+	if (file.print("{}"))
+	{
+		Serial.println("File written");
+	}
+	else
+	{
+		Serial.println("Write failed");
+	}
+	file.close();
+}
 
-            // display a file browser using spiffs on the web page
-            client.println("<!DOCTYPE html><html>");
-            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-            client.println("<link rel=\"icon\" href=\"data:,\">");
-            // CSS to style the on/off buttons
-            // Feel free to change the background-color and font-size attributes to fit your preferences
-            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-            client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
-            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-            client.println(".button2 {background-color: #77878A;}</style></head>");
-            
-            // Web Page Heading
-            client.println("<body><h1>ESP32 File Browser</h1>");
-            
-            // Check if there is any SPIFFS file
-            File root = SPIFFS.open("/");
-            File file = root.openNextFile();
-            if (!file) {
-              client.println("<h2>No files found in SPIFFS!</h2>");
-            } else {
-              client.println("<h2>Files found in SPIFFS:</h2>");
-              while (file) {
-                client.print("<a href=\"");
-                client.print(file.name());
-                client.print("\">");
-                client.print(file.name());
-                client.println("</a>");
-                file = root.openNextFile();
-              }
-            }
-            client.println("</body></html>");
+void readWood(Wood_t &wood, int id)
+{
+	File file = SPIFFS.open("/wood.json", "r");
+	// read the file in a variable
+	String fileData;
+	while (file.available())
+	{
+		fileData += char(file.read());
+	}
+	file.close();
 
-            // add a way to upload files to the SPIFFS
-            client.println("<form method=\"POST\" action=\"/upload\" enctype=\"multipart/form-data\">");
-            client.println("<input type=\"file\" name=\"data\">");
-            client.println("<input class=\"button\" type=\"submit\" value=\"Upload\">");
-            client.println("</form>");
+	if (fileData.length() > 0) // Si le fichier n'est pas vide
+	{
+		StaticJsonDocument<2048> doc; // ajuster la taille du fichier selon la quantité de bois
+		DeserializationError error = deserializeJson(doc, fileData);
 
-            // save the file to the SPIFFS
-            if (header.indexOf("GET /upload") >= 0) {
-              Serial.println("Saving file to SPIFFS");
-              File file = SPIFFS.open("/test.txt", FILE_WRITE);
-              if (!file) {
-                Serial.println("Failed to create file");
-                return;
-              }
-              if (file.print("This is a test file")) {
-                Serial.println("File was written");
-              } else {
-                Serial.println("File write failed");
-              }
-              file.close();
-            }
+		if (error)
+		{
+			Serial.print("deserializeJson() failed: ");
+			Serial.println(error.c_str());
+			return;
+		}
 
-            // The HTTP response ends with another blank line
-            client.println();
-            // Break out of the while loop
-            break;
-          } else { // if you got a newline, then clear currentLine
-            currentLine = "";
-          }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character, 
+		JsonObject root = doc.as<JsonObject>();
+		for (JsonPair keyValue : root)
+		{
+			JsonObject woodObj = keyValue.value().as<JsonObject>();
+			if (woodObj["code"].as<int>() == id)
+			{
+				strncpy(wood.name, woodObj["name"].as<const char *>(), sizeof(wood.name) - 1);
+				wood.code = woodObj["code"].as<int>();
+				wood.sawSpeed = woodObj["sawSpeed"].as<int>();
+				wood.feedRate = woodObj["feedRate"].as<int>();
+				break;
+			}
+			else
+			{
+				strncpy(wood.name, "NULL", sizeof(wood.name) - 1); // Si le bois n'est pas trouvé, retourner avec un nom NULL
+			}
+		}
+	}
+	else
+	{
+		Serial.println("Error reading JSON file data");
+	}
+}
 
-          currentLine += c;      // add it to the end of the currentLine
-        }
-      }
-    }
-    // Clear the header variable
-    header = "";
-    // Close the connection
-    client.stop();
-    Serial.println("Client disconnected.");
-    Serial.println("");
-  }
+void writeWood(Wood_t &wood, int id, int sawSpeed, int feedRate)
+{
+	// Lire les données du fichier JSON
+	File file = SPIFFS.open("/wood.json", "r");
+	// read the file in a variable
+	String fileData;
+	while (file.available())
+	{
+		fileData += char(file.read());
+	}
+	file.close();
+
+	if (fileData.length() > 0)
+	{
+		// Ajuster la taille du fichier selon la quantité de bois
+		StaticJsonDocument<2048> doc;
+		DeserializationError error = deserializeJson(doc, fileData);
+
+		if (error)
+		{
+			Serial.print("deserializeJson() failed: ");
+			Serial.println(error.c_str());
+			return;
+		}
+
+		// Créer un nouveau object JSON avec les données du bois
+		JsonObject newWood;
+		newWood["name"] = woodType[id];
+		newWood["code"] = id;
+		newWood["sawSpeed"] = sawSpeed;
+		newWood["feedRate"] = feedRate;
+
+		// Add the new JsonObject to the JsonDocument
+		JsonObject root = doc.as<JsonObject>();
+		root[String(id)] = newWood;
+		// Serialize the modified JsonDocument back to a string
+
+		// Serialize the modified JsonDocument back to a string
+		String modifiedData;
+		serializeJson(doc, modifiedData);
+
+		File file = SPIFFS.open("/wood.json", "w");
+		if (!file)
+		{
+			Serial.println("Failed to open file for writing");
+			return;
+		}
+		if (file.print(modifiedData))
+		{
+			Serial.println("File written");
+		}
+		else
+		{
+			Serial.println("Write failed");
+		}
+		file.close();
+	}
+	else
+	{
+		Serial.println("Error reading JSON file data");
+	}
+}
+
+void updateWood(Wood_t &wood, int id, int sawSpeed, int feedRate)
+{
+	// Lire le fichier de configuration
+	File file = SPIFFS.open("/wood.json", "r");
+	// read the file in a variable
+	String fileData;
+	while (file.available())
+	{
+		fileData += char(file.read());
+	}
+	file.close();
+
+	if (fileData.length() > 0)
+	{
+		// Définir la taille du fichier selon la quantité de bois
+		StaticJsonDocument<2048> doc;
+		DeserializationError error = deserializeJson(doc, fileData);
+
+		if (error)
+		{
+			Serial.print("deserializeJson() failed: ");
+			Serial.println(error.c_str());
+			return;
+		}
+
+		// Accédé à l'objet json du bois à l'aide de son id
+		JsonObject root = doc.as<JsonObject>();
+		JsonObject woodObj = root[String(id)];
+
+		// Mettre à jour les propriétés du JsonObject
+		woodObj["name"] = woodType[id];
+		woodObj["sawSpeed"] = sawSpeed;
+		woodObj["feedRate"] = feedRate;
+
+		// Serialize the modified JsonDocument back to a string
+		String modifiedData;
+		serializeJson(doc, modifiedData);
+
+		File file = SPIFFS.open("/wood.json", "w");
+		if (!file)
+		{
+			Serial.println("Failed to open file for writing");
+			return;
+		}
+		if (file.print(modifiedData))
+		{
+			Serial.println("File written");
+		}
+		else
+		{
+			Serial.println("Write failed");
+		}
+		file.close();
+	}
+	else
+	{
+		Serial.println("Error reading JSON file data");
+	}
 }
--- src/ProtecScion.ino
+++ src/ProtecScion.ino
@@ -1,4 +1,4 @@
-#include "global"
+#include "global.h"
 
 void setup()
 {
@@ -6,7 +6,7 @@ void setup()
 	Serial.begin(115200);
 
 	// Priorité 1 - Coeur 0 - Taille de pile 2048 - Lecture de la vitesse de la scie
-	xTaskCreatePinnedToCore(vTaskSoundSensor, "Capteur de son", TASK_STACK_SIZE, NULL, 1, &HandleKeypad, 0);	
+	xTaskCreatePinnedToCore(vTaskSoundSensor, "Capteur de son", TASK_STACK_SIZE, NULL, 1, &HandleSoundSensor, 0);	
 }
 
 void loop()
