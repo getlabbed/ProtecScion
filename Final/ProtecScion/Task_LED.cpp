a9a56475e9157472782530608c0c11e5ceec1178
Skkeye <38301149+Skkeye@users.noreply.github.com>
2023-05-13 16:19:43 -0400

    feat: LED mode 5: off

--- Final/ProtecScion/Task_LED.cpp
+++ Final/ProtecScion/Task_LED.cpp
@@ -2,13 +2,12 @@
 #include "Task_LED.h"
 
 void vTaskLED(void *pvParameters) {
-	LedState_t led_state = LED_BOTH;
-
-	pinMode(PIN_LED, OUTPUT);
+	LedState_t led_state = LED_OFF;
 
   while(true)
 	{
     xQueueReceive(xQueueLED, &led_state, 0);
+    pinMode(PIN_LED, (led_state == LED_OFF) ? INPUT : OUTPUT);
     digitalWrite(PIN_LED, led_state == LED_RED ? LOW : HIGH);
     vTaskDelay(pdMS_TO_TICKS(led_state == LED_BLINK ? 500 : 10));
     digitalWrite(PIN_LED, led_state == LED_GREEN ? HIGH : LOW);
